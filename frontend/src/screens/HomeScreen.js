22599230d0254012ee411267d75036e18974f2c6
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-10 22:02:04 +0530

    added product load via API

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "../components/Product";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
+import axios from "axios";
 
-const homeScreen = () => {
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get(`/api/products`);
+      setProducts(res.data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>Latest Products</h1>
@@ -25,4 +36,4 @@ const homeScreen = () => {
   );
 };
 
-export default homeScreen;
+export default HomeScreen;
