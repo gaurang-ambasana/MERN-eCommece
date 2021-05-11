c50639822ec4f1287a9a3dec1263a75f832ab8c7
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-12 01:25:29 +0530

    connected mongo + added data schema, models and some users

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcryptjs from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin",
+    email: "admin@proshop.com",
+    password: bcryptjs.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Gaurang",
+    email: "gaurang@gmail.com",
+    password: hashSync("123456", 10),
+    isAdmin: false,
+  },
+  {
+    name: "John",
+    email: "john@yahoo.com",
+    password: hashSync("123456", 10),
+    isAdmin: false,
+  },
+];
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 dotenv.config();
+
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -16,5 +21,5 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`server running up in ${process.env.NODE_ENV} on ${PORT}`);
+  console.log(`server running up in ${process.env.NODE_ENV} on ${PORT}`.yellow);
 });
