a3f37380cc5b5c85b988543257e36fa207f76cac
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-14 00:25:41 +0530

    added redux store and reducer, configured action dispatcher

---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { producytListReducer } from "./reducers/productReducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({
+  producytList: producytListReducer,
+});
+
+const intialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  intialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
