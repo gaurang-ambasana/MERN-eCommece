f4d587afa734f04a25df12a4e7fa0e4093e16a13
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-13 23:41:15 +0530

    added error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 import colors from "colors";
 
 dotenv.config();
@@ -13,7 +14,9 @@ const app = express();
 
 app.use("/api/products", productRoutes);
 
-app.use((err, req, res, next) => {});
+app.use(errorMiddleware.notFound);
+
+app.use(errorMiddleware.errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running up in ${process.env.NODE_ENV} on ${PORT}`.yellow);
