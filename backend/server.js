7a83c40db853ec5af3d0a403a3a7d5d458ce499b
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-19 00:19:45 +0530

    added user auth with jwt and profile API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 import colors from "colors";
@@ -11,8 +12,10 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(errorMiddleware.notFound);
 
