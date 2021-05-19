d2847c152ab9462bdf67360129c3a408f94afab2
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-19 20:16:32 +0530

    added new user creation api with password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,3 +41,33 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("user not found");
   }
 });
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists!!");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data! Please retry :(");
+  }
+});
