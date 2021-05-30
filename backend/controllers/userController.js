947c3725460e4d2ea70d34a136bf67dd737dd290
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-30 15:12:59 +0530

    added any user update for admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -158,3 +158,36 @@ export const deleteUser = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
+
+export const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
+
+export const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin === true || user.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
