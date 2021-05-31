1a4c9f2183ff75a40f0cdd91dc33fa0182e57991
Gaurang Ambasana <gpambasana@gmail.com>
2021-05-31 23:56:19 +0530

    added create product and edit details api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -26,3 +26,44 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error(`Product Not Found`);
   }
 });
+
+export const createProduct = asyncHandler(async (req, res) => {
+  const newProduct = new Product({
+    name: "Sample",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "sample brand",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti, voluptatibus quia qui temporibus aperiam. Aliquam officia quasi quaerat esse, quidem commodi recusandae et nulla assumenda id blanditiis! Atque, quia!",
+  });
+
+  const createdProduct = await newProduct.save();
+  res.status(201).json(createdProduct);
+});
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const thatProduct = await Product.findById(req.params.id);
+
+  if (thatProduct) {
+    thatProduct.name = name;
+    thatProduct.price = price;
+    thatProduct.description = description;
+    thatProduct.image = image;
+    thatProduct.brand = brand;
+    thatProduct.category = category;
+    thatProduct.countInStock = countInStock;
+
+    const updatedProduct = await thatProduct.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
