175749cdf63eb1509695e0a5e87d92fae13a6481
Gaurang Ambasana <gpambasana@gmail.com>
2021-06-07 23:48:17 +0530

    added product review api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -66,3 +66,42 @@ export const updateProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+export const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviwed = product.reviews.find(
+      (rev) => rev.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviwed) {
+      res.status(400);
+      throw new Error(`You've already reviwed this product!!`);
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.numReviews;
+
+    await product.save();
+
+    res.status(200).json({ message: `review is added to ${product.name}` });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
